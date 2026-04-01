cf314dc02a62cf0c22a2352bd34b1ca873b4e71a
Allen-Kristjan Päll <akpall@fst.ee>
2026-04-01 19:20:22 +0300

    define version in root instead of modules

--- providers.tf
+++ providers.tf
@@ -2,9 +2,11 @@ terraform {
   required_providers {
     libvirt = {
       source  = "dmacvicar/libvirt"
+      version = "0.9.7"
     }
     ct = {
       source  = "poseidon/ct"
+      version = "0.14.0"
     }
   }
 }
