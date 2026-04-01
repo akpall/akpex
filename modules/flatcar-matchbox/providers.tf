01ea1dd38c004708432869f7318eb582ee646717
Allen-Kristjan Päll <akpall@fst.ee>
2026-04-01 19:49:24 +0300

    format providers

--- modules/flatcar-matchbox/providers.tf
+++ modules/flatcar-matchbox/providers.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
     libvirt = {
-      source  = "dmacvicar/libvirt"
+      source = "dmacvicar/libvirt"
     }
     ct = {
-      source  = "poseidon/ct"
+      source = "poseidon/ct"
     }
   }
 }
