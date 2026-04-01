79b52c9b7d9300323308a536af68c1a8c8e47948
Allen-Kristjan Päll <akpall@fst.ee>
2026-04-01 23:00:56 +0300

    kubernetes

---
+++ kubernetes/providers.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "3.0.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "3.1.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "config"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "config"
+  }
+}
