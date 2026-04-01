d77a1edc16aa1d01f5dddc67485ace72a4f42e73
Allen-Kristjan Päll <akpall@fst.ee>
2026-04-01 20:28:06 +0300

    rework main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,27 @@
 locals {
-  etcd-nodes = { for i in range(1, 3) : "flatcar-node${i}" => "" }
-  nodes      = { for i in range(3, 8) : "flatcar-node${i}" => "" }
+  flatcar-etcd-nodes   = { for i in range(1, 3) : "flatcar-node${i}" => "" }
+  flatcar-worker-nodes = { for i in range(3, 8) : "flatcar-node${i}" => "" }
 }
 
 module "flatcar-network" {
-  source = "./modules/flatcar-network"
-
+  source                     = "./modules/flatcar-network"
   network_name               = "flatcar_network"
   network_ip_dhcp_ranges_end = "192.168.100.253"
 }
 
 module "flatcar-matchbox" {
-  source = "./modules/flatcar-matchbox"
-
+  source  = "./modules/flatcar-matchbox"
   vm_name = "flatcar-node0"
 }
 
-module "flatcar-etcd" {
-  source   = "./modules/flatcar-node"
-  for_each = local.etcd-nodes
-
-  disk_capacity_bytes = 20 * 1024 * 1024 * 1024
-  vm_name             = each.key
+module "flatcar-etcd-nodes" {
+  source   = "./modules/flatcar-etcd-node"
+  for_each = local.flatcar-etcd-nodes
+  vm_name  = each.key
 }
 
-module "flatcar-nodes" {
-  source   = "./modules/flatcar-node"
-  for_each = local.nodes
-
-  vm_name = each.key
+module "flatcar-worker-nodes" {
+  source   = "./modules/flatcar-worker-node"
+  for_each = local.flatcar-worker-nodes
+  vm_name  = each.key
 }
