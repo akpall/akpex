3bbe9441fd29116adc81841489574fcfc5f5eab2
Allen-Kristjan Päll <akpall@fst.ee>
2026-04-20 07:18:42 +0300

    cleanup locals.tf

--- libvirt-nodes/locals.tf
+++ libvirt-nodes/locals.tf
@@ -5,13 +5,7 @@ locals {
 
   flatcar_etcd_init_nodes = {
     "flatcar-node0" = {
-      cidr                = 24
       disk_capacity_bytes = 20 * 1024 * 1024 * 1024
-      dns                 = "192.168.100.1"
-      gateway             = "192.168.100.1"
-      interface           = "eth0"
-      ip                  = "192.168.100.2"
-      keepalived_priority = 100
       mac_address         = "52:54:00:00:00:00"
       memory              = 2048
       vcpu                = 2
@@ -20,25 +14,13 @@ locals {
 
   flatcar_etcd_join_nodes = {
     "flatcar-node1" = {
-      cidr                = 24
       disk_capacity_bytes = 20 * 1024 * 1024 * 1024
-      dns                 = "192.168.100.1"
-      gateway             = "192.168.100.1"
-      interface           = "eth0"
-      ip                  = "192.168.100.3"
-      keepalived_priority = 90
       mac_address         = "52:54:00:00:00:01"
       memory              = 2048
       vcpu                = 2
     }
     "flatcar-node2" = {
-      cidr                = 24
       disk_capacity_bytes = 20 * 1024 * 1024 * 1024
-      dns                 = "192.168.100.1"
-      gateway             = "192.168.100.1"
-      interface           = "eth0"
-      ip                  = "192.168.100.4"
-      keepalived_priority = 80
       mac_address         = "52:54:00:00:00:02"
       memory              = 2048
       vcpu                = 2
@@ -56,8 +38,8 @@ locals {
   flatcar_matchbox_nodes = {
     "flatcar-matchbox-node" = {
       disk_capacity_bytes = 20 * 1024 * 1024 * 1024
-      vcpu                = 2
       memory              = 2048
+      vcpu                = 2
     }
   }
 }
